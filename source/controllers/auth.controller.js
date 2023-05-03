66c65ce8f1f0e42f02c74967dab29b90779ad8bc
Renan Neves <renan.epd@gmail.com>
2023-05-03 11:31:36 -0300

    tests

--- source/controllers/auth.controller.js
+++ source/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import authService from '../services/auth.service.js';
 import jwt from 'jsonwebtoken';
 
 const login = async (req, resp) => {
-
     try {
         const { userName, senhaUser } = req.body;
         const usuario = await authService.login(userName, senhaUser);
@@ -10,7 +9,7 @@ const login = async (req, resp) => {
         if (!usuario) {
             return resp.status(400).json({ message: 'Usuário ou Senha Incorretos' });
         }
-        const token = jwt.sign({ payload: usuario._id }, process.env.SECRET, { expiresIn: '1m' });
+        const token = jwt.sign({ payload: usuario._id }, process.env.SECRET, { expiresIn: '1h' });
         return resp.status(200).json({ message: 'Login Efetuado', token });
     }
     catch (erro) {
