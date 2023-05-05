a897f8a8b5bc51e697c779b0ccc0d9963c7b047a
Renan Neves <renan.epd@gmail.com>
2023-05-05 20:28:03 -0300

    commit the refactor

--- source/controllers/client.controller.js
+++ source/controllers/client.controller.js
@@ -60,6 +60,8 @@ const insertClient = async (req, resp) => {
         if (!cnpj || !razao || !telefone || !contato || !situacao) {
             return resp.status(401).json({ message: 'Campo obrigatório não informado' })
         }
+        if (!validaCnpj(cnpj))
+            return resp.status(400).json({ message: 'CNPJ informado está incorreto' })
         await clientService.insertClientService(cliente)
         return resp.status(201).json({ message: 'Cliente Cadastrado com sucesso!' })
     } catch (error) {
@@ -96,7 +98,6 @@ const updateStatusClient = async (req, resp) => {
 
 const updateClient = async(req, resp) => {
     try {
-        const cliCnpj = req.params.cnpj;
         const { cnpj, razao, telefone, contato, alerta, situacao } = req.body
         const cliente = {
             cnpj,
